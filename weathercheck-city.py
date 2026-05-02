597ceebf62b25966afa119c4057e0cfc5fac3a6e
sabin99shrestha <fireynan@gmail.com>
2026-05-02 13:00:45 +0545

    Remove print statement for API key

--- weathercheck-city.py
+++ weathercheck-city.py
@@ -5,7 +5,7 @@ import os
 load_dotenv("api.env")
 url = "https://api.openweathermap.org/data/2.5/weather"
 api_key=os.getenv("API_KEY")
-print(api_key)
+
 name=input("Enter your city name: ")
 args = {
     "q": name ,
